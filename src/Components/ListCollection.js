c63c453d48636be5fa646cf433250c383a2c92f4
Chris <cvo13014@gmail.com>
2024-04-05 14:35:40 -0700

    added functional search bar, no styling

--- src/Components/ListCollection.js
+++ src/Components/ListCollection.js
@@ -1,13 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 /* Component for Collection of Todo Objects */
 const ListCollection = ({ lists, title }) => {
   // note: lists reversed (non-destructive) to display most recents first
+  const [blogWanted, setBlogWanted] = useState(null);
+
+  const listsWanted = lists.filter(
+    (list) => list.name.toLowerCase() === blogWanted.toLowerCase()
+  );
+
+  let listsRecentsFirst = lists.toReversed();
+  if (Object.keys(listsWanted).length !== 0) {
+    listsRecentsFirst = listsWanted.toReversed();
+  }
 
   return (
     <div className="collection">
       <h1>{title}</h1>
-      {lists.toReversed().map((list) => (
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => {
+          setBlogWanted(e.target.value);
+        }}
+      />
+      {listsRecentsFirst.map((list) => (
         <div className="list-preview" key={list.id}>
           <Link to={'/lists/' + list.id}>
             <div className="title">
